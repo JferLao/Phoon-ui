0953b7319e1be88bad542fced5b92a8518c8f463
jferlao <438127468@qq.com>
2020-11-23 14:15:56 +0800

    change version to 0.2.0

--- build/bin/version.js
+++ build/bin/version.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 var path = require('path');
 var version = process.env.VERSION || require('../../package.json').version;
-var content = { '1.0.0':'1.0.0'}
-if (!content[version]) content[version] = '1.0';
+var content = { '0.2.0':'0.2.0'}
+if (!content[version]) content[version] = '0.2.0';
 
 fs.writeFileSync(path.resolve(__dirname,'../../examples/versions.json'),JSON.stringify(content));   //写入当前版本号
